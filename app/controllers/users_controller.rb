cdd89c6fb95daacedfd3bc86326cb0b130737cf8
Andriychyk <andrii.romaniuk-ist201@nung.edu.ua>
2022-11-25 21:42:25 +0200

    data b2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:First_name, :Last_name, :Phone, :Email, :Address)
+      params.require(:user).permit(:First_name, :Last_name, :Phone, :Email, :Address, :userrole_id)
     end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :First_name, :Last_name, :Phone, :Email, :Address, :created_at, :updated_at
+json.extract! user, :id, :First_name, :Last_name, :Phone, :Email, :Address, :created_at, :updated_at, :userrole_id
 json.url user_url(user, format: :json)
